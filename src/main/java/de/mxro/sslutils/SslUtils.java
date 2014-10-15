964b5ddd515d2639f3a67ca49aa0a6c3e2cb542f
mxro <mxro@nowhere.com>
2014-10-15 11:29:44 +1100

    Assembling SSL Utilities project Autocommit Wed Oct 15 11:29:44 EST 2014

--- src/main/java/de/mxro/sslutils/SslUtils.java
+++ src/main/java/de/mxro/sslutils/SslUtils.java
@@ -1,5 +1,28 @@
 package de.mxro.sslutils;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+
 public class SslUtils {
+
+    public static Map<SslKeyStoreData, SSLContext> cache;
+
+    static {
+        cache = new IdentityHashMap<SslKeyStoreData, SSLContext>();
+    }
+
+    public static SSLContext createContextForCertificate(final SslKeyStoreData keyStoreData) {
+        if (cache.containsKey(keyStoreData)) {
+            return cache.get(keyStoreData);
+        }
+
+        final SSLContext newContext = SslContextFactory.getServerContext(keyStoreData);
+
+        cache.put(keyStoreData, newContext);
+
+        return newContext;
+    }
 
 }
